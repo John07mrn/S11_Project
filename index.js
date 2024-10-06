84447f31f32daa4a58118e42c510162fba939a21
John07mrn <john1992miron@gmail.com>
2024-10-06 14:39:13 +0100

    add Conditionals & Loops

--- index.js
+++ index.js
@@ -65,3 +65,93 @@ for (const word of words) {
     console.log(word);
 }
 
+/**
+* Palindrome Check:
+* Write a program that checks if a word
+* is a palindrome (reads the same from left to right and right to left, ignoring spaces and case differences)
+*/
+let wordToCheck = 'a bba';
+wordToCheck = wordToCheck.toLowerCase().replace(/\s/g, ''); // Convert to lowercase and remove spaces
+const reverse = wordToCheck.split('').reverse().join('');
+const isPalindromeMessage = `The word ${wordToCheck} is a palindrome`;
+const notPalindromeMessage = `The word ${wordToCheck} is not a palindrome`;
+wordToCheck === reverse ? console.log(isPalindromeMessage) : console.log(notPalindromeMessage);
+
+/**
+ * Calculate the sum of the squares of numbers from 1 to 10.
+ */
+let sumSquares = 0;
+
+for (let i = 1; i <= 10; i++) {
+    sumSquares += i * i;
+}
+
+console.log(`The sum of the squares of numbers from 1 to 10 is: ${sumSquares}`);
+
+/**
+ * Determine Parity:
+ * Given an array const nums = [1, 12, 30, 21]
+ * Write a program that displays a message
+ * indicating whether each number in the array is even or odd.
+ */
+const nums = [1, 12, 30, 21];
+
+for (let i = 0; i < nums.length; i++) {
+    if (nums[i] % 2 === 0) {
+        console.log(`${nums[i]} is an even number.`);
+    } else {
+        console.log(`${nums[i]} is an odd number.`);
+    }
+}
+
+/**
+ * Prime Numbers in Range:
+ * Given two numbers x = 12, y = 35, display all prime numbers between x and y.
+ */
+const x = 12;
+const y = 35;
+console.log(`Prime numbers in the range ${x} - ${y}:`);
+for (let i = x; i <= y; i++) {
+    let prime = true;
+    for (let j = 2; j <= Math.sqrt(numberToCheck); j++) {
+        if (i % j === 0) {
+            prime = false;
+            break;
+        }
+    }
+    if (prime) {
+        console.log(i);
+    }
+}
+
+/**
+ * Display Multiplication Table:
+ * Display the multiplication table for numbers from 1 to 10.
+ */
+console.log("Multiplication table:");
+
+for (let i = 1; i <= 10; i++) {
+    for (let j = 1; j <= 10; j++) {
+        console.log(`${i} * ${j} = ${i * j}`);
+    }
+    console.log(); // Add a blank line between each row for clarity
+}
+
+/**
+ * FizzBuzz Game:
+ * Write a program that displays the numbers from 1 to 50.
+ * For every multiple of 3, display "Fizz".
+ * For every multiple of 5, display "Buzz".
+ * For numbers that are multiples of both 3 and 5, display "FizzBuzz".
+ */
+for (let i = 1; i <= 50; i++) {
+    if (i % 3 === 0 && i % 5 === 0) {
+        console.log("FizzBuzz");
+    } else if (i % 3 === 0) {
+        console.log("Fizz");
+    } else if (i % 5 === 0) {
+        console.log("Buzz");
+    } else {
+        console.log(i);
+    }
+}
